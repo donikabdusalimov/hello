7d914385384ff5f3962aa0fd67225450c57dafaa
Donik <126549201+donikabdusalimov@users.noreply.github.com>
2023-04-05 14:24:42 +0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,16 @@
+// // task1
+// let arr = ['morgen', 'nikolay', 'alexander', 'yulduz', 'timati'];
+// let longestName = '';
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i].length > longestName.length) {
+//         longestName = arr[i];
+//     }
+// }
+// console.log(`Самое длинное имя в массиве: ${longestName}`);
+
+// task2
+// let numbers = [2, 4, 1, 4, 7, 3, 2, 7, 9, 3, 2]
+// let even = numbers.filter((item) => item % 2 === 0)
+// console.log(even);
+// let odd = numbers.filter(item => item % 2)
+// console.log(odd);
